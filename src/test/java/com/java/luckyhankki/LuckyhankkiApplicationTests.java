2719b13c6f6d07597d2c42f3a9c86b8f16eb64ac
Euijeong Hwang <euijeong.hwang97@gmail.com>
2025-05-05 00:22:35 +0900

    feat: 스프링부트 프로젝트 템플릿 추가
    - 스프링부트 프로젝트 템플릿 생성
    
    Related to : #5

---
+++ src/test/java/com/java/luckyhankki/LuckyhankkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.luckyhankki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyhankkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
