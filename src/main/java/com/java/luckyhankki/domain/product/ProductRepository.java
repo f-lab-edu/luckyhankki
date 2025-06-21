223a97f6ab95acf1fbb95fcbcfb105c5ebd80f27
Euijeong Hwang <euijeong.hwang97@gmail.com>
2025-06-21 23:56:42 +0900

    feat: 상품 조회 조건을 QueryDSL 기반으로 동적 처리하도록 구현
    - 동적 쿼리 사용 및 학습 목적으로 QueryDSL 도입
    - 상품 조회 조건을 동적 처리하도록 변경
      - 카테고리 ID, 픽업 날짜, 키워드, 정렬 기준
    - 상품 조회 조건이 있는 API와 조회 조건이 없는 전체 상품 조회 API 분리
      - 정렬 기준은 현재 priceDiscount ASC 기준
      - 추후 거리가 가까운 순으로 정렬하도록 변경

--- src/main/java/com/java/luckyhankki/domain/product/ProductRepository.java
+++ src/main/java/com/java/luckyhankki/domain/product/ProductRepository.java
@@ -1,13 +1,13 @@
 package com.java.luckyhankki.domain.product;
 
-import com.java.luckyhankki.dto.ProductResponse;
+import com.java.luckyhankki.dto.product.ProductResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
     //해당 가게의 비활성화된 상품도 함께 조회
     List<ProductResponse> findAllByStoreId(Long storeId);
 
